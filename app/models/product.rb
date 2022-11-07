901c6c8101a691e12e460945b13d02b3eac8cbe7
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-11-07 15:16:54 +0300

    completed assignment

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,20 @@
 class Product < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, user_id: user.id, product_id: self.id)
+    end
+
+    def self.print_all_reviews
+      puts revies.map do |review| 
+        "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+      end
+    end
+
+    def self.average_rating
+        reviews.average(:star_rating).to_f
+    end
 end
+
+))
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def self.favourite_product
+        # self.products.maximum(:star_rating)
+        reviews.order(:star_rating).last.product
+    end
+
+    def self.remove_reviews(product)
+        # product.reviews.filter{|review| review.user_id==self.id}
+        # .map{||review|review.destroy}
+        reviews.where(product: product).destroy_all
+    end
 end
