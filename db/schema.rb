db3be09aa5974e719acdbabcb3c9efd9579ccf8b
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-11-07 23:35:19 +0300

    Added solution

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_172042) do
+ActiveRecord::Schema.define(version: 2022_11_07_120113) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.integer "star_rating"
+    t.string "comment"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,24 +3,24 @@
 puts "Deleting old data..."
 Product.destroy_all
 User.destroy_all
-
 puts "Creating users..."
 user1 = User.create(name: Faker::Name.name)
 user2 = User.create(name: Faker::Name.name)
 user3 = User.create(name: Faker::Name.name)
-
 puts "Creating products..."
 product1 = Product.create(name: "Stapler", price: 10)
 product2 = Product.create(name: "Whiteboard", price: 15)
 product3 = Product.create(name: "Dry Erase Markers", price: 5)
 product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
-
 puts "Creating reviews..."
 # *****************************************************************
 # * TODO: create reviews! Remember, a review belongs to a product *
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
-
+review1 = Review.create(star_rating: 10, comment: "excellent", user_id: 1, product_id: 5)
+review2 = Review.create(star_rating: 5, comment: "good", user_id: 2, product_id: 5)
+review3 = Review.create(star_rating: 7, comment: "Neutral", user_id: 3, product_id: 3)
+review4 = Review.create(star_rating: 3, comment: "poor", user_id: 2, product_id: 1)
 puts "Seeding done!"
